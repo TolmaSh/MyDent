65ebb0a8c044eece7839010cb75538ed32d84a0c
TolmaSh <m0v4anua@gmail.com>
2022-07-11 03:23:01 +0300

    feat: resume work with project

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,20 @@
 import {testFunction} from './scripts/test';
 testFunction();
+
+function initMap() {
+  const myLatLng = { lat: 50.48521042270771, lng: 30.489357336582913 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 18,
+    center: myLatLng,
+    mapTypeControl: false,
+    fullscreenControl: false,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+
+}
+window.initMap = initMap;
