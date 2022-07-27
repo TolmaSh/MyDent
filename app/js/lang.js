29792a5ffd840ee8f707dbece539cf964a653bbe
TolmaSh <m0v4anua@gmail.com>
2022-07-27 15:00:03 +0300

    feat: translate header, general , reasons section

---
+++ app/js/lang.js
@@ -0,0 +1,90 @@
+export const langArr = {
+  'header_main': {
+    'ua': 'Головна',
+    'eng': 'Main'
+  },
+  'header_about': {
+    'ua': 'Про клініку',
+    'eng': 'About clinic'
+  },
+  'header_doctors': {
+    'ua': 'Лікарі',
+    'eng': 'Doctors'
+  },
+  'header_services': {
+    'ua': 'Послуги',
+    'eng': 'Services'
+  },
+  'header_contacts': {
+    'ua': 'Контакти',
+    'eng': 'Contacts'
+  },
+  'header_consultation': {
+    'ua': 'Безкоштовна консультація',
+    'eng': 'Free consultation'
+  },
+  // GENERAL SECTION
+  'general_title': {
+    'ua': 'Лікуємо зуби без болю та страху',
+    'eng': 'We treat teeth without pain and fear'
+  },
+  'general_subtitle': {
+    'ua': `Допоможемо здобути усмішку мрії. Запишіться на перший прийом онлайн та отримайте знижку <span>30%</span> на повну діагностику
+        ротової порожнини.`,
+    'eng': `Let us help you shine the smile of your dreams. Book your first appointment online and get  <span>30%</span>  discount for full diagnostics
+        oral cavity.`
+  },
+  'general_enroll': {
+    'ua': 'Записатись',
+    'eng': 'Sign up'
+  },
+  'general_services': {
+    'ua': 'Послуги',
+    'eng': 'Services'
+  },
+//  REASONS SECTION
+  'reasons_title': {
+    'ua': `<span class="reasons__title_bold">5</span> причин звернутись до нас`,
+    'eng': '<span class="reasons__title_bold">5</span> reasons to contact us'
+  },
+  'reasons_item-titleFirst': {
+    'ua': `Сімейна стоматологія`,
+    'eng': 'Family dentistry'
+  },
+  'reasons_item-textFirst': {
+    'ua': `Ми не просто стоматологія, а маленька сім'я. За нашим життям можна спостерігати в Instagram`,
+    'eng': 'We are not just a dentistry, but a small family. You can follow our life on Instagram'
+  },
+  'reasons_item-titleSecond': {
+    'ua': `Технологічний підхід`,
+    'eng': 'Technological approach'
+  },
+  'reasons_item-textSecond': {
+    'ua': `Використовуємо якісний матеріал і інструмент. Відмовляємось від застарілих методик лікування в пользу інноваційних способах`,
+    'eng': 'We use quality material and tools. We abandon outdated methods of treatment in favor of innovative methods'
+  },
+  'reasons_item-titleThird': {
+    'ua': `Дружня атмосфера`,
+    'eng': 'Friendly atmosphere'
+  },
+  'reasons_item-textThird': {
+    'ua': `Для нас пацієнт – це не просто клієнт. Всі наші пацієнти становляться друзями і хорошими знайомими`,
+    'eng': 'For us, a patient is not just a client. All our patients become friends and good acquaintances'
+  },
+  'reasons_item-titleFourth': {
+    'ua': `Тільки необхідне лікування`,
+    'eng': 'Only necessary treatment'
+  },
+  'reasons_item-textFourth': {
+    'ua': `Ми не нав'язуємо додаткові послуги, якщо не бачимо в цьому сенсу. Нам важливо дати пацієнтові більше, ніж взяти від нього`,
+    'eng': 'We do not impose additional services if we do not see the sense in this. It is important for us to give the patient more than to take from him'
+  },
+  'reasons_item-titleFifth': {
+    'ua': `Лікуємо дорослих та дітей`,
+    'eng': 'We treat adults and children'
+  },
+  'reasons_item-textFifth': {
+    'ua': `Зазвичай як дорослих, так і маленьких пацієнтів. Для цього ми маємо всі необхідні знання, а головне – правильний підхід до дітей`,
+    'eng': 'Usually, both adults and small patients. For this, we have all the necessary knowledge, and most importantly, the right approach to children'
+  },
+}
--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,39 @@
-import {testFunction} from './scripts/test';
-testFunction();
+// import {testFunction} from './scripts/test';
+// testFunction();
+import {langArr} from './lang';
+
+const changeLangButtons = document.querySelectorAll('.general__language-item');
+const langEl = document.querySelector('.general__language')
+const allLang = ['ua','eng']
+
+
+
+// CHANGE ACTIVE BUTTON FRO CLICK , START CHANGE LANGUAGE FUNCTION , ADD HASH ON PATHNAME
+changeLangButtons.forEach(e => {
+  let lang = e.dataset.lang;
+  console.log(e)
+  e.addEventListener('click', () => {
+    langEl.querySelector('.general__language-item--active').classList.remove('general__language-item--active');
+    e.classList.add('general__language-item--active');
+    location.href = window.location.pathname + '#' + lang;
+    changeLanguage()
+  })
+})
+
+// FUNCTION GET AND CHANGED TEXT FROM LANG.JS
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#ua';
+  }
+  for (let key in langArr) {
+    document.querySelector('.lng-' + key).innerHTML = langArr[key][hash];
+  }
+}
+
+
+
 
 function initMap() {
   const myLatLng = { lat: 50.48521042270771, lng: 30.489357336582913 };
